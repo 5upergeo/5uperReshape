84151f4930d6a9bb02f189e533fbd93d84b5dc88
lamperder <lamperder@edboy.cc.cc>
2017-11-16 10:56:54 +0800

    Remove unimportant

--- draft.py
+++ draft.py
@@ -6,7 +6,7 @@ import shapefile
 import math
 
 # variables
-path = "G:/Github/5uperReshape/test_file/"
+path = "FolderPath"
 polyline = "COASTLINE_clip.shp"
 polygon = "COASTLINE_Polygon.shp"
 newPolygon = "COASTLINE_Polygon_New.shp"
@@ -30,23 +30,9 @@ pl_end_closest = len(s_plg.points)
 pl_start_closest_distance = math.inf
 pl_end_closest_distance = math.inf
 
-print(s_plg.parts)
+# print(s_plg.parts)
 s_plg_parts = s_plg.parts
 s_plg_parts2 = list(s_plg_parts[1:]) + [len(s_plg.points)]
-
-# for i in range(0, len(s_plg.points)):
-#         distance_start = math.hypot(s_plg.points[i][0] - pl_start[0], s_plg.points[i][1] - pl_start[1])
-#         distance_end = math.hypot(s_plg.points[i][0] - pl_end[0], s_plg.points[i][1] - pl_end[1])
-#         if distance_start < pl_start_closest_distance:
-#                 pl_start_closest = i
-#                 pl_start_closest_distance = distance_start
-#
-#         if distance_end < pl_end_closest_distance:
-#                 pl_end_closest = i
-#                 pl_end_closest_distance = distance_end
-#
-# print(pl_start_closest)
-# print(pl_end_closest)
 
 partIndex = 0
 for i in range(0, len(s_plg_parts)):
@@ -65,16 +51,12 @@ for i in range(0, len(s_plg_parts)):
                         pl_end_closest = j
                         pl_end_closest_distance = distance_end
 
-print(pl_start_closest)
-print(pl_end_closest)
-
+# print(pl_start_closest)
+# print(pl_end_closest)
 
 s_plg.points = s_plg.points[0:pl_start_closest] + s_pl.points + s_plg.points[pl_end_closest+1:]
 partOffset = pl_end_closest - pl_start_closest + 1 - len(s_pl.points)
 for i in range(partIndex, len(s_plg_parts)):
         s_plg_parts[i] = s_plg_parts[i] - partOffset
 
-
-# s_plg.points = s_plg.points[0:366266] + [(0,0)] + s_plg.points[366266:]
-#
 plg.save(path + newPolygon)
